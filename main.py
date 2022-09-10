e0e5b7794c96feeb122640e4b1dd2ca3794f5762
Mathieu Beliveau <mathieu.beliveau.1@gmail.com>
2022-09-10 12:44:21 -0400

    use generators to prevent loading all data in memory

--- main.py
+++ main.py
@@ -36,19 +36,26 @@ def generate_glare_coordinates():
     opposite_y = math.cos(opposite_deg) * end_length + (target_height / 2)
     return init_x, init_y, opposite_x, opposite_y
 
-
-def get_dataset():
-    source_img_tensors = []
-    target_img_tensors = []
+def get_dataset_from_generator():
     dir_list = os.listdir(source_folder)
-    for file in dir_list:
+    return tf.data.Dataset.from_generator(image_tensor_generator, args=[len(dir_list)],
+                                          output_signature=(tf.TensorSpec(shape=(600, 800, 3), dtype=tf.float32),
+                                                            tf.TensorSpec(shape=(600, 800, 3), dtype=tf.float32)))
+
+def image_tensor_generator(stop):
+    dir_list = os.listdir(source_folder)
+    i = 0
+    while i < stop:
+        file = dir_list[i]
         source_img = tf.io.read_file(source_folder + file)
         target_img = tf.io.read_file(target_folder + file)
-        source_img_tensors.append(tf.cast(tf.image.decode_jpeg(source_img), tf.float32) / 255.0)
-        target_img_tensors.append(tf.cast(tf.image.decode_jpeg(target_img), tf.float32) / 255.0)
-    return tf.data.Dataset.from_tensor_slices((source_img_tensors, target_img_tensors))
+        source_tensor = tf.cast(tf.image.decode_jpeg(source_img), tf.float32) / 255.0
+        target_tensor = tf.cast(tf.image.decode_jpeg(target_img), tf.float32) / 255.0
+        yield source_tensor, target_tensor
 
 
 if __name__ == '__main__':
-    generate_dataset()
-    #dataset = get_dataset()
+    #generate_dataset()
+    dataset = get_dataset_from_generator()
+    for elem in dataset:
+        print(elem.numpy())
